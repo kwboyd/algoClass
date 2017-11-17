25fa22b1a2536f1dc7818f57e9e4f47183efbbc2
kwboyd <kwboyd@live.unc.edu>
2017-11-16 21:55:25 -0500

    Added basic fib attempt and solution

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -8,3 +8,17 @@ output   0 1 1 2 3 5 8 13 21 34 ...
 
 What is the time complexity? Can you think of optimizing your solution? (Hint: look up dynamic programming)
 */
+
+// attempt
+
+var fib = function(n) {
+  if (n <= 1) return n;
+  return fib(n) + fib(n-1); // nope, n-1 and n-2
+}
+
+// solution
+
+function fibonnaci (n) {
+  if (n === 0 || n === 1) return n;
+  return fibonnaci(n-1) + fibonnaci(n-2);
+}
